ef8a81ad4fdac77579d7ece61a4b6841a19a1eec
lotheren <Firedragon11!>
2014-12-25 16:52:59 -0800

    This is commit 1 adding LogInFrame and User class

--- LoginFrame.java
+++ LoginFrame.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Kaigen on 12/7/14.
@@ -11,7 +13,7 @@ public class LoginFrame {
     JPanel buttonPanel;
     JTextArea text;
     JButton loginButton;
-    JButton cancleButton;
+    JButton cancelButton;
     JButton createButton;
 
     /**
@@ -23,9 +25,41 @@ public class LoginFrame {
     public LoginFrame(Model model) {
         text = new JTextArea("Please enter ID or hit create.");
         loginButton = new JButton("Login");
-        cancleButton = new JButton("Cancel");
+        // Make Button do someting.
+        /*TODO create new window?
+
+         */
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        cancelButton = new JButton("Cancel");
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
         createButton = new JButton("Create");
         constructPanel();
+
+    }
+    public void createUser(){
+        String name;
+        int id;
+
+        JPanel panel = new JPanel();
+        JFrame frame = new JFrame("Create User");
+        JLabel nameLabel = new JLabel("Select Name");
+        JLabel passWord = new JLabel("Select Password");
+        JTextArea nameTextArea = new JTextArea("Please select your name");
+        JTextArea passWordTextArea = new JTextArea("Please select your password");
+
+
+
+
 
     }
 
@@ -34,14 +68,13 @@ public class LoginFrame {
      * for user to log in.
      */
 
-
     public void constructPanel(){
         frame = new JFrame("Login");
         frame.setSize(400,400);
         frame.setLocationRelativeTo(null);
         buttonPanel = new JPanel();
         buttonPanel.add(loginButton);
-        buttonPanel.add(cancleButton);
+        buttonPanel.add(cancelButton);
         buttonPanel.add(createButton);
         frame.add(text, BorderLayout.CENTER);
         frame.add(buttonPanel, BorderLayout.SOUTH);
---
+++ User.java
@@ -0,0 +1,14 @@
+/**
+ * Created by kaigen on 12/25/14.
+ * Holds the User data
+ * String Name
+ * Int ID
+ */
+public class User {
+    String name;
+    int id;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
